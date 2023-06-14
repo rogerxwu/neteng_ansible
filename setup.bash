c494d845a93f7e6129f2ae4b1636033605aca111
Xian Wu <xianwuusa@gmail.com>
2023-06-13 20:35:52 -0700

    init

--- setup.bash
+++ setup.bash
@@ -20,4 +20,6 @@ python3.10 -m virtualenv venv
 cd neteng_ansible
 source venv/bin/activate
 pip install -r requirements.txt
+
+# 
 ansible-galaxy collection install networktocode.nautobot:4.3.1 --force
